4d99f28afbd94863a567bb286cfa0a39793d1694
Tyrant <tyler.sauve@bag-of-holding.com>
2025-03-09 19:41:41 -0700

    Finish tutorial

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def train(rnn, training_data, n_epoch = 10, n_batch_size = 64, report_every = 50
 
         all_losses.append(current_loss / len(batches))
         if iter % report_every == 0:
-            print(f"{iter} ({iter / n_epoch:.0%}: \t average batch loss = {all_losses[-1]}")
+            print(f"epoch {iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
         current_loss = 0
 
     return all_losses
@@ -177,3 +177,49 @@ start = time.time()
 all_losses = train(rnn, train_set, n_epoch=27, learning_rate=0.15, report_every=5)
 end = time.time()
 print(f"completed training in {(end - start) * 1000:.1f}ms")
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+
+def evaluate(rnn, testing_data, classes):
+    confusion = torch.zeros(len(classes), len(classes))
+
+    rnn.eval()  # set to eval mode
+    with torch.no_grad():
+        for i in range(len(testing_data)):
+            (label_tensor, text_tensor, label, text) = testing_data[i]
+            output = rnn(text_tensor)
+            guess, guess_i = label_from_output(oupput, classes)
+            label_i = classes.index(label)
+            confusion[label_i][guess_i] += 1
+
+    # Normalize by dividing every row by its sum
+    for i in range(len(classes)):
+        denom = confusion[i].sum()
+        if denom > 0:
+            confusion[i] = confusion[i] / denom
+
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(confusion.cpu().numpy())  # numpy uses cpu here so we need to use a cpu version
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticks(np.arange(len(classes)), labels=classes, rotation=90)
+    ax.set_yticks(np.arange(len(classes)), labels=classes)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # sphinx_gallery_thumbnail_number = 2
+    plt.show()
+
+
+evaluate(rnn, test_set, classes=alldata.labels_uniq)
